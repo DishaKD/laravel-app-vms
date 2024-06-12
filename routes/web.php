352201f2ea03992a0eb534df6e90c4296b27bb41
DishaKD <dasanayakedishal@gmail.com>
2024-06-13 03:04:27 +0530

    Route Implement for Update

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [HomeController::class, "index"]) -> name ('home');
+Route::get('/', [HomeController::class, "index"])->name('home');
 
-Route::get('/addProduct', [AddProductController::class, "index"]) -> name ('add');
+Route::get('/addProduct', [AddProductController::class, "index"])->name('add');
 
 
-Route::prefix('/product')->group(function (){
-    Route::get('/dashboard', [ProductController::class, "index"]) -> name ('productsView');
-    Route::post('/store', [ProductController::class, "store"]) -> name ('product.store');
-    Route::get('/{product_id}/delete', [ProductController::class, "delete"]) -> name ('product.delete');
+Route::prefix('/product')->group(function () {
+    Route::get('/dashboard', [ProductController::class, "index"])->name('productsView');
+    Route::post('/store', [ProductController::class, "store"])->name('product.store');
+    Route::post('/{product_id}/update', [ProductController::class, "update"])->name('product.update');
+    Route::get('/{product_id}/delete', [ProductController::class, "delete"])->name('product.delete');
 });
