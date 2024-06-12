82bd6efc0639e0fd3e52b874c033d2603266d0c4
DishaKD <dasanayakedishal@gmail.com>
2024-06-12 16:40:25 +0530

    Add jetstream library

--- resources/views/libraries/styles.blade.php
+++ resources/views/libraries/styles.blade.php
@@ -1,4 +1,6 @@
+@livewireStyles
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <!-- Boostrap Icon CDN -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
 
-    <body class="h-100 text-center text-white bg-dark">
+        <main class="px-3">
+            <h1>Warehouse Management System</h1>
+            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit
+                the text, and add your own fullscreen background photo to make it your own.</p>
 
-        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Warehouse">
+                </div>
+                <div class="col-md-6">
+                    <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Management">
+                </div>
+            </div>
 
+            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit
+                the text, and add your own fullscreen background photo to make it your own.</p>
+            <p class="lead">
+                <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+            </p>
+        </main>
 
-            <main class="px-3 ">
-                <h1>Warehouse Management System</h1>
-                <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit
-                    the text, and add your own fullscreen background photo to make it your own.</p>
-
-                <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit
-                    the text, and add your own fullscreen background photo to make it your own.</p>
-                <p class="lead">
-                    <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
-                </p>
-            </main>
-
-
-        </div>
-
-    </body>
+    </div>
 @endsection
