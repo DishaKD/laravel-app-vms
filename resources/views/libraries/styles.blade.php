3cf0e1c824e4185a51cb8183551ca338eaf992cc
DishaKD <dasanayakedishal@gmail.com>
2024-06-12 13:01:11 +0530

    Bootstrap Icon CDN added

--- resources/views/libraries/styles.blade.php
+++ resources/views/libraries/styles.blade.php
@@ -1,2 +1,4 @@
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<!-- Boostrap Icon CDN -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
