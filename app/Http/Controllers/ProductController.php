9c7e85e37a79cba3dad9ae9894c77ed8c55994cc
DishaKD <dasanayakedishal@gmail.com>
2024-06-13 03:02:59 +0530

    Implement Update function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,32 +11,39 @@ class ProductController extends ParentController
 
     public function index()
     {
-        $response ['products'] = ProductFacade::index();
+        $response['products'] = ProductFacade::index();
         return view('pages.productDashboard')->with($response);
     }
 
     public function store(Request $request)
-{
-    ProductFacade::store([
-        'product_title' => $request->input('product_title'),
-        'product_sku' => $request->input('product_sku'),
-        'category' => $request->input('category'),
-        'quantity' => $request->input('quantity'),
-        'order_from' => $request->input('order_from'),
-        'order_by' => $request->input('order_by'),
-        'contact_info' => $request->input('contact_info'),
-        'special_instructions' => $request->input('special_instructions'),
-        'status' => 0
-    ]);
+    {
+        ProductFacade::store([
+            'product_title' => $request->input('product_title'),
+            'product_sku' => $request->input('product_sku'),
+            'category' => $request->input('category'),
+            'quantity' => $request->input('quantity'),
+            'order_from' => $request->input('order_from'),
+            'order_by' => $request->input('order_by'),
+            'contact_info' => $request->input('contact_info'),
+            'special_instructions' => $request->input('special_instructions'),
+            'status' => 0
+        ]);
 
-    return redirect()->back();
-}
+        return redirect()->back();
+    }
 
 
     public function delete($product_id)
     {
         ProductFacade::delete($product_id);
 
+        return redirect()->back();
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        ProductFacade::update($request->all(), $id);
         return redirect()->back();
     }
 }
--- domain/Services/ProductService.php
+++ domain/Services/ProductService.php
@@ -7,7 +7,7 @@ use App\Models\Product;
 class ProductService
 {
 
-       protected $product;
+    protected $product;
 
     public function __construct()
     {
@@ -19,26 +19,48 @@ class ProductService
         return $this->product->all();
     }
 
-    public function store($data)
-{
-    $this->product->create([
-        'product_title' => $data['product_title'],
-        'product_sku' => $data['product_sku'],
-        'category' => $data['category'],
-        'quantity' => $data['quantity'],
-        'order_from' => $data['order_from'],
-        'order_by' => $data['order_by'],
-        'contact_info' => $data['contact_info'],
-        'special_instructions' => $data['special_instructions'],
-        'status' => 0
-    ]);
-}
-
-
-    public function delete($product_id)
+    public function get($product_id)
     {
-        $product = $this->product->find($product_id);
+        return $this->product->find($product_id);
+    }
+
+    public function store($data)
+    {
+        $this->product->create([
+            'product_title' => $data['product_title'],
+            'product_sku' => $data['product_sku'],
+            'category' => $data['category'],
+            'quantity' => $data['quantity'],
+            'order_from' => $data['order_from'],
+            'order_by' => $data['order_by'],
+            'contact_info' => $data['contact_info'],
+            'special_instructions' => $data['special_instructions'],
+            'status' => 0
+        ]);
+    }
+
+
+    public function delete($id)
+    {
+        $product = $this->product->find($id);
         $product->delete();
     }
 
+    public function update(array $data, $id)
+    {
+        $product = $this->product->find($id);
+
+        if ($product) {
+            $product->update([
+                'product_title' => $data['product_title'],
+                'product_sku' => $data['product_sku'],
+                'category' => $data['category'],
+                'quantity' => $data['quantity'],
+                'order_from' => $data['order-from'],
+                'order_by' => $data['order-by'],
+                'contact_info' => $data['contact-info'],
+                'special_instructions' => $data['special-instructions'],
+            ]);
+        }
+    }
 }
