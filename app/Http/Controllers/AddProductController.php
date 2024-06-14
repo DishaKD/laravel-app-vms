632af76b488ba308d47620ab41ade138838691d1
DishaKD <dasanayakedishal@gmail.com>
2024-06-14 12:11:05 +0530

    Extend Add Product with parentcontroller

--- app/Http/Controllers/AddProductController.php
+++ app/Http/Controllers/AddProductController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AddProductController extends Controller
+class AddProductController extends ParentController
 {
     public function index()
-   {
-    return view('pages.addProduct');
-   }
+    {
+        return view('pages.addProduct');
+    }
 }
