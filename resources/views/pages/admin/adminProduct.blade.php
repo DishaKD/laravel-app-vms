3599b326477e0ae341dcc364a08b447f13455779
DishaKD <dasanayakedishal@gmail.com>
2024-06-17 14:14:26 +0530

    Create Table for Display Added Products

--- resources/views/pages/admin/adminProduct.blade.php
+++ resources/views/pages/admin/adminProduct.blade.php
@@ -22,53 +22,6 @@
             </a>
         </div>
 
-        <div class="form-container">
-            <form>
-                <!-- 2 column grid layout with text inputs for the first and last names -->
-                <div class="row mb-4">
-                    <div class="col">
-                        <div data-mdb-input-init class="form-outline">
-                            <input type="text" id="form6Example1" class="form-control" />
-                            <label class="form-label" for="form6Example1">First name</label>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div data-mdb-input-init class="form-outline">
-                            <input type="text" id="form6Example2" class="form-control" />
-                            <label class="form-label" for="form6Example2">Last name</label>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Text input -->
-                <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="text" id="form6Example3" class="form-control" />
-                    <label class="form-label" for="form6Example3">Company name</label>
-                </div>
-
-                <!-- Text input -->
-                <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="text" id="form6Example4" class="form-control" />
-                    <label class="form-label" for="form6Example4">Address</label>
-                </div>
-
-                <!-- Email input -->
-                <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="email" id="form6Example5" class="form-control" />
-                    <label class="form-label" for="form6Example5">Email</label>
-                </div>
-
-                <!-- Number input -->
-                <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="number" id="form6Example6" class="form-control" />
-                    <label class="form-label" for="form6Example6">Phone</label>
-                </div>
-
-                <!-- Submit button -->
-                <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4">Place order</button>
-            </form>
-        </div>
-
         <!-- Table Section -->
         <table class="table align-middle mb-0 bg-white">
             <thead class="bg-light">
